5ce477c8c059ba73e720f7e95f4de7c8162314ce
Jany Battung <janybattung@gmail.com>
2020-08-26 13:26:47 -0700

    add files

---
+++ src/Components/ListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+    render() {
+        return (
+            <div className='ListItem'>
+                <h2>List Item</h2>
+            </div>
+        )
+    }
+}
+export default ListItem;
